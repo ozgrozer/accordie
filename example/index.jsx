0c4861d7d909c83a01b588ba750685cb5cdac9a2
Ozgur <ozgr@live.com>
2022-01-23 19:24:30 +0300

    Add regular CSS example

--- example/index.jsx
+++ example/index.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Accordie, Panel } from './../build/Accordie.js'
+import { Accordie, Panel } from './../src/Accordie'
 
-const Index = () => {
+const RegularCSS = () => {
   return (
-    <div>
-      <Accordie>
-        <Panel
-          Heading='heading1'
-          Content='content1'
-        />
+    <>
+      <style>
+        {`
+          .panel {
+            margin-top: 10px;
+          }
+          .panel.open .content {
+            display: block;
+          }
+          .panel.close .content {
+            display: none;
+          }
+          .heading {
+            padding: 10px;
+            background-color: #ddd;
+          }
+          .content {
+            padding: 10px;
+          }
+        `}
+      </style>
 
-        <Panel
-          Heading={<div>heading2</div>}
-          Content={<div>content2</div>}
-        />
-      </Accordie>
-    </div>
+      <div>
+        <Accordie
+          classList={{
+            panel: 'panel',
+            heading: 'heading',
+            content: 'content',
+            open: 'open',
+            close: 'close'
+          }}
+        >
+          <Panel
+            Heading='heading 1'
+            Content='content 1'
+          />
+
+          <Panel
+            Heading='heading 2'
+            Content='content 2'
+          />
+        </Accordie>
+      </div>
+    </>
   )
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'))
+ReactDOM.render(<RegularCSS />, document.getElementById('root'))
