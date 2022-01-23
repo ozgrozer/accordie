39a3db33445487bd7b4a13768633fa87ab7058b8
Ozgur <ozgr@live.com>
2022-01-24 02:40:37 +0300

    Build v0.0.4

--- build/Accordie.js
+++ build/Accordie.js
@@ -1,6 +1,6 @@
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import styles from './styles.module.css';
 import { MainContext, MainProvider } from './MainContext';
 
@@ -14,6 +14,11 @@ const Panel = props => {
     classList,
     ...otherProps
   } = props;
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef(null);
+  useEffect(() => {
+    setContentHeight(contentRef.current.scrollHeight);
+  }, []);
   const {
     state,
     setState
@@ -43,12 +48,17 @@ const Panel = props => {
   const panelClassName = customStyle ? clx(classList.panel, panelIsOpen ? classList.open : classList.close) : clx(styles.panel, panelIsOpen ? styles.open : styles.close);
   const headingClassName = customStyle ? classList.heading : styles.heading;
   const contentClassName = customStyle ? classList.content : styles.content;
+  const contentStyle = {
+    height: panelIsOpen ? contentHeight : 0
+  };
   return /*#__PURE__*/React.createElement("div", _extends({}, otherProps, {
     className: panelClassName
   }), /*#__PURE__*/React.createElement("div", {
     onClick: toggleContent,
     className: headingClassName
   }, Heading), /*#__PURE__*/React.createElement("div", {
+    ref: contentRef,
+    style: contentStyle,
     className: contentClassName
   }, Content));
 };
