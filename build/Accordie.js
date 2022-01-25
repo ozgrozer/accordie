48b175bd2d27abb14fcd58bec20bbb5cc4802e61
Ozgur <ozgr@live.com>
2022-01-25 11:49:12 +0300

    Update filename

--- build/Accordie.js
+++ build/Accordie.js
@@ -1,7 +1,7 @@
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 import React, { useContext, useState, useEffect, useRef } from 'react';
-import styles from './styles.module.scss';
+import styles from './styles.module.css';
 import { MainContext, MainProvider } from './MainContext';
 
 const clx = (...classes) => classes.join(' ');
