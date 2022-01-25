5825c38ffbd49635d24a6fe13ba01e5772b449af
Ozgur <ozgr@live.com>
2022-01-25 11:10:04 +0300

    Update examples

--- example/index.jsx
+++ example/index.jsx
@@ -5,9 +5,33 @@ import './styles.css'
 import styles from './styles.module.scss'
 import { Accordie, Panel } from './../src/Accordie'
 
+const NoCustomStyle = () => {
+  return (
+    <>
+      <div className='title'>No Custom Style</div>
+
+      <div className='accordie'>
+        <Accordie>
+          <Panel
+            Heading='Heading 1'
+            Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
+          />
+
+          <Panel
+            Heading='Heading 2'
+            Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
+          />
+        </Accordie>
+      </div>
+    </>
+  )
+}
+
 const RegularCSS = () => {
   return (
     <>
+      <div className='title'>Regular CSS</div>
+
       <div className='accordie'>
         <Accordie
           classList={{
@@ -35,38 +59,43 @@ const RegularCSS = () => {
 
 const CSSModules = () => {
   return (
-    <div className={styles.accordie}>
-      <Accordie
-        classList={{
-          panel: styles.panel,
-          heading: styles.heading,
-          content: styles.content,
-          open: styles.open,
-          close: styles.close
-        }}
-      >
-        <Panel
-          Heading={<div>Heading 1</div>}
-          Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
-        />
+    <>
+      <div className='title'>CSS Modules</div>
 
-        <Panel
-          Heading={<div>Heading 2</div>}
-          Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
-        />
+      <div className={styles.accordie}>
+        <Accordie
+          classList={{
+            panel: styles.panel,
+            heading: styles.heading,
+            content: styles.content,
+            open: styles.open,
+            close: styles.close
+          }}
+        >
+          <Panel
+            Heading={<div>Heading 1</div>}
+            Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
+          />
 
-        <Panel
-          Heading={<div>Heading 3</div>}
-          Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
-        />
-      </Accordie>
-    </div>
+          <Panel
+            Heading={<div>Heading 2</div>}
+            Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
+          />
+
+          <Panel
+            Heading={<div>Heading 3</div>}
+            Content={<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>}
+          />
+        </Accordie>
+      </div>
+    </>
   )
 }
 
 const App = () => {
   return (
     <div style={{ width: '500px', margin: 'auto' }}>
+      <NoCustomStyle />
       <RegularCSS />
       <CSSModules />
     </div>
